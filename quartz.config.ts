e61a2cfe450bebd6cfdd0706acc1d7e54e58f869
theo-blogical <theoblogical@gmail.com>
2026-02-02 19:29:11 -0800

    Quartz sync: Feb 2, 2026, 2:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Westgate Small Group",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
