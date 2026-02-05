023bf243a3ebec090482ef1d88459d5d8957a266
Sanborn-Young <theoblogical@gmail.com>
2026-02-04 21:07:56 -0800

    fix heading

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "HardDriveGuy's Refer",
+    pageTitle: "HardDriveGuy's Web Site",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
