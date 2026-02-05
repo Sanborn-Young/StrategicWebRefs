06c55156937a3d6696f1ed24da4dd6d140ebf72f
Sanborn-Young <sanborn.young@gmail.com>
2026-02-04 16:46:54 -0800

    Initial Quartz import for StrategicWebRefs

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Westgate Small Group",
+    pageTitle: "HardDriveGuy Subreddits",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
